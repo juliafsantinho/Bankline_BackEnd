a9948967d3ed3c789502e20606ed983a9ab32d6f
Julia Santinho da Fonseca <90910965+juliafsantinho@users.noreply.github.com>
2022-05-05 22:35:54 -0300

    Controller e Repository

--- src/main/java/com/dio/santander/banklineapi/model/Movimentacao.java
+++ src/main/java/com/dio/santander/banklineapi/model/Movimentacao.java
@@ -26,6 +26,9 @@ public class Movimentacao {
 	
 	private Double valor;
 	
+	@Column(name = "id_conta")
+	private Integer idConta;
+	
 	@Enumerated(EnumType.STRING)
 	private MovimentacaoTipo tipo;
 	
@@ -59,6 +62,14 @@ public class Movimentacao {
 	}
 	public void setTipo(MovimentacaoTipo tipo) {
 		this.tipo = tipo;
+	}
+	
+	public Integer getIdConta() {
+		return idConta;
+	}
+	
+	public void setIdConta(Integer idConta) {
+		this.idConta = idConta;
 	}
 	
 	
